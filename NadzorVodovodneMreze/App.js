cadcec1a7b3856fb6aa8ece9c3851d5107634b5a
llemes <lamija.lemes@gmail.com>
2018-05-13 11:14:25 +0200

    Urađen template za navigaciju

--- NadzorVodovodneMreze/App.js
+++ NadzorVodovodneMreze/App.js
@@ -5,24 +5,33 @@ import { StackNavigator } from 'react-navigation';
 
 import helpcomponent from './components/help-component/help-component';
 import styles from './AppStyles';
+import userinterfacecomponent from './components/user-interface-component/user-interface-component';
 
 class maincomponent extends Component {
+	constructor(props) {
+		super(props);
+		this.onPress = this.onPress.bind(this);
+	}
 	static navigationOptions = 	{
-    		title: 'Stranica za prijavu',
- 	};
-	onClickHelp = () => {
-    		this.props.navigation.navigate('Second');  
- 	}
+		title: 'Stranica za prijavu',
+	};
+
+	onPress(txt) {
+		this.props.navigation.navigate(txt);
+	} 
+
 	render() {
 		return(
 		<View style = { styles.container }> 
-			<Button onPress = { this.onClickHelp } title = 'Help' style = { styles.button } />
+			<Button onPress = { () => this.onPress('UserInterface') } title = 'User Interface ' style = { styles.button } />
+			<Button onPress = { () => this.onPress('Help') } title = 'Help' style = { styles.button } />
 		</View>
 		);
 	}
 }
 
 export default Project = StackNavigator({
-	First: { screen: maincomponent },
-	Second: { screen: helpcomponent }
+	Main: { screen: maincomponent },
+	Help: { screen: helpcomponent },
+	UserInterface: { screen: userinterfacecomponent }
 });
--- NadzorVodovodneMreze/components/user-interface-component/user-interface-component.js
+++ NadzorVodovodneMreze/components/user-interface-component/user-interface-component.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 import styles from './styles';
-import {AppRegistry} from 'react-native';
+import { AppRegistry, View, Text } from 'react-native';
 
-export default class userinterfacecomponent {
+export default class userinterfacecomponent extends Component {
     static navigationOptions = {
         title: 'Igrica'
     }
     render() {
         return (
             <View>
+                <Text> UI component works </Text>
             </View>
         );
     }
