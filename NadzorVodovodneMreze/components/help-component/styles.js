c6d6ba8f539b2556ed6128612e7e353b6620c55e
ehlymana <ekrupalija1@etf.unsa.ba>
2018-05-16 20:54:29 +0200

    Commit funkcionalne Help komponente.

--- NadzorVodovodneMreze/components/help-component/styles.js
+++ NadzorVodovodneMreze/components/help-component/styles.js
@@ -2,22 +2,52 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: '#fff',
+		flexDirection: 'column',
+		backgroundColor: 'white',
 		alignItems: 'center',
 		justifyContent: 'center', 
 		margin: 20,
 		borderWidth: 0,
 		padding: 5
 	},
-	button : {
-		borderWidth: 1,
-		borderColor: 'black',
-		color: 'black'
+	button: {
+		padding: 10,
+		alignItems: 'stretch'
 	},
 	text : {
-		fontSize: 14,
-		textAlign: 'left'
+		fontSize: 18,
+		textAlign: 'left',
+		padding : 10
+	},
+	textH : {
+		fontSize: 18,
+		textAlign: 'left',
+		padding : 10,
+		fontWeight : 'bold'
+	},
+	header : {
+		backgroundColor: 'grey',
+		padding: 5,
+		margin: 0
+	},
+	zasebniView : {
+		backgroundColor: 'white',
+		padding: 5
+	},
+	images : {
+		padding: 10,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	image : {
+		width: 50,
+		height: 50
+	},
+	textB : {
+		fontSize: 18,
+		textAlign: 'left',
+		padding : 10,
+		fontWeight : 'bold'
 	}
 });
 
